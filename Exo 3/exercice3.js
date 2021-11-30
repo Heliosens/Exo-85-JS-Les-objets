d59950f307034886e6b246af3c16c02f6ffe1187
Sylvie-B <unsolo@hotmail.fr>
2021-11-30 11:40:42 +0100

    affiche les proprietés

--- Exo 3/exercice3.js
+++ Exo 3/exercice3.js
@@ -1,13 +1,13 @@
-let personne = {
-    "prenom" : "James",
-    "nom" : "Bond",
-           :    ,
-            :
+const Personne = {
+    prenom : "James",
+    nom : "Bond",
+    metier : "agent secret",
+    nomDeCode :"007"
 };
 
 /* Afficher ci dessous "mon nom est <propriété nom de notre objet>, <propriété prenom de notre objet>
  <propriété nom de notre objet>, agent <propriété nomDeCode de notre objet>" dans un alert */
 
-alert();
+alert("mon nom est " + Personne.nom + ", " + Personne.prenom + " " + Personne.nom + ", agent " + Personne.nomDeCode);
 
 
